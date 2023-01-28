2bb36dc9616d1270f39c890d66ccf68bf6fea716
Amirhossein Zarei <103286828+AmirZarei2002@users.noreply.github.com>
2023-01-28 17:35:49 +0330

    some styles changed

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ import {
 } from '../sections';
 
 const Page = () => (
-    <div className='bg-slate-800 overflow-hidden px-10'>
+    <div className="bg-primary-black overflow-hidden">
         <Navbar />
         <Hero />
         <About />
